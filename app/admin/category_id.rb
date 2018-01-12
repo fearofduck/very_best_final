40be6ddc26d69a02268c58054517b82864152236
Raghu Betina <raghu@firstdraft.com>
2018-01-12 03:28:22 +0000

    Whitelist columns in CategoryId for ActiveAdmin

--- app/admin/category_id.rb
+++ app/admin/category_id.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register CategoryId do
 
+ permit_params :name, :cuisine
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
